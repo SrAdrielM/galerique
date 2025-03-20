f3ad487ca49970864779ddf89a2d598dbebdb4d9
SrAdrielM <20230542@ricaldone.edu.sv>
2025-03-19 20:59:38 -0600

    da shi

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import reviewsRouter from "./src/routes/reviews.js";
 import ordersRouter from "./src/routes/orders.js"
 import buyerRouter from "./src/routes/buyer.js"
 import sellerRouter from "./src/routes/seller.js"
+import salesRouter from "./src/routes/sales.js"
 
 const app = express();
 
@@ -13,5 +14,6 @@ app.use("/api/reviews", reviewsRouter);
 app.use("/api/orders", ordersRouter);
 app.use("/api/buyer", buyerRouter);
 app.use("/api/seller", sellerRouter);
+app.use("/api/sales", salesRouter);
 
 export default app;
