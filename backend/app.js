11e1818e28149a6d6157e20945b535da5226143b
SrAdrielM <20230542@ricaldone.edu.sv>
2025-03-19 22:21:58 -0600

    aaaa

--- backend/app.js
+++ backend/app.js
@@ -1,38 +1,25 @@
 import express from "express";
 
 import reviewsRouter from "./src/routes/reviews.js";
-<<<<<<< HEAD
 import ordersRouter from "./src/routes/orders.js"
 import buyerRouter from "./src/routes/buyer.js"
 import sellerRouter from "./src/routes/seller.js"
-<<<<<<< HEAD
-=======
 import productsRouter from "./src/routes/products.js";
 import categoriesRouter from "./src/routes/categories.js";
 import techniqueRouter from "./src/routes/techniques.js";
->>>>>>> kevin
-=======
 import salesRouter from "./src/routes/sales.js"
->>>>>>> adriel
 
 const app = express();
 
 app.use(express.json());
 
-<<<<<<< HEAD
-app.use("/api/reviews", reviewsRouter);
 app.use("/api/orders", ordersRouter);
 app.use("/api/buyer", buyerRouter);
 app.use("/api/seller", sellerRouter);
-<<<<<<< HEAD
-=======
 app.use("/api/reviews", reviewsRouter)
+app.use("/api/sales", salesRouter)
 app.use("/api/products", productsRouter)
 app.use("/api/categories", categoriesRouter)
 app.use("/api/techniques", techniqueRouter)
->>>>>>> kevin
-=======
-app.use("/api/sales", salesRouter);
->>>>>>> adriel
 
 export default app;
